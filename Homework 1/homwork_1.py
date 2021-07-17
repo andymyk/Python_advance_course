8502476a95fbfc981bd4c01fc189bca942c35bba
andymyk <andriymykytyak@gmail.com>
2021-07-17 23:31:15 +0200

    - update

--- Homework 1/homwork_1.py
+++ Homework 1/homwork_1.py
@@ -7,29 +7,21 @@
 ранее сумме и после этого завершить программу.
 """
 
-suma = list()
 
 def main():
-    some_text = input('Enter numbers separated by a space: ').split()
-    some_func(some_text)
-
-def some_func(some_text):
     total = 0
     count = 0
-    for i in some_text:
-        if i.isdigit():
-            total += int(i)
-        else:
-            count += 1
-    return check(total, count)
-
-
-def check(total, count):
-    suma.append(total)
-    if count > 0:
-        return print(f'The sum of entered numbers: {sum(suma)}')
-    else:
-        return print(f'The sum of entered numbers: {sum(suma)}'), main()
+    while True:
+        some_text = input('Enter numbers separated by a space: ').split()
+        for i in some_text:
+            if i.isdigit():
+                total += int(i)
+            else:
+                count += 1
+        print(total)
+        if count > 0:
+            break
+    return total
 
 
 if __name__ == "__main__":
