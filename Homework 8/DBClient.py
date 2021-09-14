7a4257981e66a653cc9a508558bef0f209b1e6b7
Andriy <carjevich@gmail.com>
2021-09-14 17:40:25 +0200

    дз8

---
+++ Homework 8/DBClient.py
@@ -0,0 +1,42 @@
+from congif import host, user, password, db_name
+import psycopg2
+
+
+class DBClient:
+    CHECK_ACTIVE = """SELECT COUNT(status) FROM orders WHERE status = 'Active'"""
+    CHECK_PERSONAL = """SELECT personal_data, department_name FROM employees e LEFT JOIN departments d"""
+    CHECK_DATA = """SELECT * FROM orders WHERE created_dt = '2021-09-07'"""
+    CHECK_ID_ORDERS = """SELECT order_id, personal_data FROM orders o
+LEFT JOIN employees e ON(o.creator_id = e.employee_id)"""
+
+
+    def __init__(self):
+        self.connect = None
+
+    def setup(self):
+        self.connect = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+
+    def check_status_a(self):
+        with self.connect.cursor() as cursor:
+            cursor.execute(self.CHECK_ACTIVE)
+            self.connect.commit()
+
+    def check_p(self):
+        with self.connect.cursor() as cursor:
+            cursor.execute(self.CHECK_PERSONAL)
+            self.connect.commit()
+
+    def check_d(self):
+        with self.connect.cursor() as cursor:
+            cursor.execute(self.CHECK_DATA)
+            self.connect.commit()
+
+    def check_id_o(self):
+        with self.connect.cursor() as cursor:
+            cursor.execute(self.CHECK_ID_ORDERS)
+            self.connect.commit()
---
+++ Homework 8/hw_bot.py
@@ -0,0 +1,45 @@
+from telebot import TeleBot
+from envparse import Env
+from DBClient import DBClient
+
+env = Env()
+TOKEN = env.str("TOKEN")
+bot = TeleBot(TOKEN)
+
+
+class BotApi:
+
+    MENU_STRING = "\n1 - работа с базой\n"
+
+    def __init__(self, DBClient):
+        self.DBClient = DBClient
+
+    def setup_all_components(self):
+        self.DBClient.setup()
+
+
+    def check_active_status(self):
+        self.DBClient.check_status_a()
+
+    def check_personal(self):
+        self.DBClient.check_p()
+
+    def run(self):
+        self.setup_all_components()
+
+        choice_mapper = {"1": self.setup_all_components,
+                         "2": self.check_active_status,
+                         "3": self.check_personal}
+
+        while True:
+            user_choice = input(f"Введите желаемое действие: {self.MENU_STRING}или q для выхода: ")
+            if user_choice.lower() == "q":
+                break
+            elif user_choice == "1":
+                user_choice = input(f"1 - вывести все книги\n2 - добавить новую книгу\n3 - удалить книгу по id ")
+                choice_mapper[user_choice]()
+
+        print("Спасибо за пользованием нашим софтом!")
+
+
+BotApi(DBClient=DBClient).run()
