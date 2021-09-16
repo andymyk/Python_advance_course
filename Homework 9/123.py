40c6a87c4dfcf3b3e23d1c10f70d4d45632fbe59
Andriy <carjevich@gmail.com>
2021-09-16 16:12:18 +0200

    дз9

---
+++ Homework 9/123.py
@@ -0,0 +1,37 @@
+from multiprocessing import Process
+from datetime import datetime
+
+class A:
+    def first(self):
+        s = [i for i in range(10 ** 8)]
+    spis = []
+    def __call__(self):
+        for i in range(3):
+            start = datetime.now()
+            A.first(self)
+            finish = datetime.now()
+            res = finish - start
+
+            print(f'Child: {res}')
+
+class B:
+    def first(self):
+        s = [i for i in range(10 ** 8)]
+    spis = []
+    def __call__(self):
+        for i in range(3):
+            start = datetime.now()
+            B.first(self)
+            finish = datetime.now()
+            res = finish - start
+            print(f'Main: {res}')
+
+
+if __name__ == '__main__':
+    a = A()
+    b = B()
+
+    p1 = Process(target=a)
+    p1.start()
+    p1.join()
+    b()
---
+++ Homework 9/hw.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from threading import Thread
+
+def first():
+    start = datetime.now()
+    s = [i for i in range(10 ** 8)]
+    finish = datetime.now()
+    res = finish - start
+    print(f'Potoki: {res}')
+
+
+def second():
+    s = [i for i in range(10 ** 8)]
+
+def func():
+    for i in range(3):
+        start = datetime.now()
+        second()
+        finish = datetime.now()
+        res = finish - start
+        print(f'Function: {res}')
+
+th = Thread(target=first)
+th2 = Thread(target=first)
+th3 = Thread(target=first)
+th.start()
+th2.start()
+th3.start()
+func()
