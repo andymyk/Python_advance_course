984e6af4dc639713e48b565dc1238f1b31174469
Andriy <carjevich@gmail.com>
2021-09-17 16:56:13 +0200

    update

--- Homework 8/DBClient.py
+++ Homework 8/DBClient.py
@@ -3,20 +3,16 @@ import psycopg2
 
 
 class DBClient:
-    CHECK_ACTIVE = """SELECT COUNT(status) FROM orders WHERE status = 'Active'"""
-    CHECK_PERSONAL = """SELECT personal_data, department_name FROM employees e LEFT JOIN departments d"""
+    CHECK_ACTIVE = """SELECT COUNT(status) FROM orders WHERE status = 'In proces'"""
+    CHECK_PERSONAL = """SELECT personal_data,department_name FROM employees JOIN departments ON 
+employees.department_id = departments.department_id;"""
     CHECK_DATA = """SELECT * FROM orders WHERE created_dt = '2021-09-07'"""
-    CHECK_ID_ORDERS = """SELECT order_id, personal_data FROM orders o
-LEFT JOIN employees e ON(o.creator_id = e.employee_id)"""
+    CHECK_ID_ORDERS = """SELECT order_id, personal_data FROM orders
+JOIN employees ON(orders.creator_id = employees.employee_id)"""
 
 
     def __init__(self):
-        self.connect = psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=db_name
-        )
+        self.connect = None
 
     def setup(self):
         self.connect = psycopg2.connect(
@@ -28,20 +24,24 @@ LEFT JOIN employees e ON(o.creator_id = e.employee_id)"""
 
     def check_status_a(self):
         with self.connect.cursor() as cursor:
-            cursor.execute(self.CHECK_ACTIVE)
+            cursor.execute(DBClient.CHECK_ACTIVE)
+            print(cursor.fetchall())
             self.connect.commit()
 
     def check_p(self):
         with self.connect.cursor() as cursor:
-            cursor.execute(self.CHECK_PERSONAL)
+            cursor.execute(DBClient.CHECK_PERSONAL)
+            print(cursor.fetchall())
             self.connect.commit()
 
     def check_d(self):
         with self.connect.cursor() as cursor:
-            cursor.execute(self.CHECK_DATA)
+            cursor.execute(DBClient.CHECK_DATA)
+            print(cursor.fetchall())
             self.connect.commit()
 
     def check_id_o(self):
         with self.connect.cursor() as cursor:
-            cursor.execute(self.CHECK_ID_ORDERS)
+            cursor.execute(DBClient.CHECK_ID_ORDERS)
+            print(cursor.fetchall())
             self.connect.commit()
--- Homework 8/hw_bot.py
+++ Homework 8/hw_bot.py
@@ -1,7 +1,7 @@
 from telebot import TeleBot
 from envparse import Env
 from DBClient import DBClient
-#
+
 env = Env()
 TOKEN = env.str("TOKEN")
 bot = TeleBot(TOKEN)
@@ -15,31 +15,38 @@ class BotApi:
         self.DBClient = DBClient
 
     def setup_all_components(self):
-        self.DBClient.setup()
+        self.DBClient.setup(self)
 
 
     def check_active_status(self):
-        self.DBClient.check_status_a()
+        self.DBClient.check_status_a(self)
 
     def check_personal(self):
-        self.DBClient.check_p()
+        self.DBClient.check_p(self)
+
+    def check_order(self):
+        self.DBClient.check_d(self)
+
+    def check_id(self):
+        self.DBClient.check_id_o(self)
 
     def run(self):
         self.setup_all_components()
 
-        choice_mapper = {"1": self.setup_all_components,
-                         "2": self.check_active_status,
-                         "3": self.check_personal}
+        choice_mapper = {"1": self.check_active_status,
+                         "2": self.check_personal,
+                         "3": self.check_order,
+                         "4": self.check_id}
 
         while True:
             user_choice = input(f"Введите желаемое действие: {self.MENU_STRING}или q для выхода: ")
             if user_choice.lower() == "q":
                 break
             elif user_choice == "1":
-                user_choice = input(f"1 - вывести все книги\n2 - добавить новую книгу\n3 - удалить книгу по id ")
+                user_choice = input(f"1 - Показать активные заявки\n2 - Перечень сотрудников и департаментов, в которых они работают"
+                                    f"\n3 - количество заявок за '2021-09-07'\n4 - перечень заявок и ФИО сотрудников, которые их создали ")
                 choice_mapper[user_choice]()
 
         print("Спасибо за пользованием нашим софтом!")
-
 
 BotApi(DBClient=DBClient).run()
